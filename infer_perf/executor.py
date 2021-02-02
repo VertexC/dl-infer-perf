27b1af5f76e1d61a390cf06f8525bae914d36014
VertexC <bob2420083992@gmail.com>
2021-02-02 13:58:58 +0800

    set up and organize task from json file

---
+++ infer_perf/executor.py
@@ -0,0 +1,95 @@
+import time
+import json
+
+from tf2xla import tf2xla_runner
+
+# TODO: enlarge to proper value
+# set to small for now
+WARM_UP_STEPS = 5
+STEPS = 10
+
+
+# TODO: add single
+class Task:
+    def __init__(self, name, model, batch_size, params):
+        self.name = name
+        self.model = model
+        self.batch_size = batch_size
+        self.params = params
+
+    def get_runner(self):
+        if self.name == 'tf2xla':
+            return tf2xla_runner(self.model, self.batch_size, **self.params)
+
+    def __str__(self):
+        return 'name:{} model:{} batch_size:{} params:{}'.format(
+            self.name, self.model, self.batch_size, self.params)
+
+
+def validate_config(config):
+    if 'models' not in data or len(data['models']) == 0:
+        return "Missing models", False
+    if 'batch_sizes' not in data or len(data['batch_sizes']) == 0:
+        return "Missg batch_sizes", False
+    if 'runners' not in data or len(data['runners']) == 0:
+        return "Missing runners", False
+    return "", True
+
+
+def generate_tasks(config):
+    tasks = []
+    for model in config['models']:
+        for batch_size in config['batch_sizes']:
+            for runner in config['runners']:
+                for name, params in runner.items():
+                    tasks.append(Task(name, model, batch_size, params))
+                    break
+    return tasks
+
+
+def benchmark_executor(config):
+    msg, valid = validate_config(config)
+    if not valid:
+        raise Exception("Invlida benchmark config : {}".format(msg))
+    tasks = generate_tasks(config)
+    import pdb
+    pdb.set_trace()
+    benchmark = create_benchmark()
+    report = []
+    for task in tasks:
+        runner = task.get_runner()
+        duration = benchmark(task.name, runner)
+        metric = "{}: {}".format(str(task), duration)
+        print(metric)
+        report.append(metric)
+    print(report)
+
+
+def create_benchmark(data_size=256):
+    def benchmark(name, runner, data_size=256):
+        for i in range(WARM_UP_STEPS):
+            runner(data_size)
+        avg_time = 0
+        for i in range(STEPS):
+            tic = time.time()
+            runner(data_size)
+            toc = time.time()
+            avg_time += (toc - tic)
+            print("running {} round {} duration: {:.2f}".format(
+                name, i, toc - tic))
+        avg_time /= STEPS
+        return avg_time,
+
+    return benchmark
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="benchmark task runner")
+    parser.add_argument("file", type=str, help="tf model name")
+
+    arg = parser.parse_args()
+    with open(arg.file) as f:
+        data = json.load(f)
+
+    benchmark_executor(data)
--- infer_perf/tf2xla.py
+++ infer_perf/tf2xla.py
@@ -8,11 +8,14 @@ from util import tf_keras_model
 
 WARM_UP_STEPS = 5
 
+
 def tf2xla_runner(model_name, batch_size=1, xla=False, device='gpu'):
     if xla:
         tf.keras.backend.clear_session()
         tf.config.optimizer.set_jit(True)    # Enable XLA
-
+    else:
+        tf.keras.backend.clear_session()
+        tf.config.optimizer.set_jit(False)
     if device == 'cpu':
         os.environ['CUDA_VISIBLE_DEVICES'] = ''
     else:
@@ -20,25 +23,34 @@ def tf2xla_runner(model_name, batch_size=1, xla=False, device='gpu'):
             del os.environ['CUDA_VISIBLE_DEVICES']
     model = tf_keras_model(model_name)
     data = np.random.rand(batch_size, 224, 224, 3).astype(np.float32)
+
     def runner(data_size):
-        for _ in range(data_size//batch_size):
+        for _ in range(data_size // batch_size):
             ret = model.predict(data, batch_size=batch_size)
 
     return runner
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="benchmark of tf/xla")
     parser.add_argument("model", help="tf model name")
-    parser.add_argument(
-        "--xla", action='store_true', help='Flag to turn on xla')
-    parser.add_argument(
-        "--device",
-        choices=['gpu', 'cpu'],
-        default='cpu',
-        help='device to run on')
+    parser.add_argument("--xla",
+                        action='store_true',
+                        help='Flag to turn on xla')
+    parser.add_argument("--device",
+                        choices=['gpu', 'cpu'],
+                        default='cpu',
+                        help='device to run on')
     parser.add_argument("--batch", type=int, default=1, help='batch size')
     arg = parser.parse_args()
 
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
-    runner = tf2xla_runner(arg.model, batch_size=arg.batch)
+    runner = tf2xla_runner(arg.model,
+                           batch_size=arg.batch,
+                           xla=arg.xla,
+                           device=arg.device)
+    tic = time.time()
+    runner(256)
+    toc = time.time()
+    duration = toc - tic
+    print(duration)
