af95015981f8def1d0a43f79ee1aa6e5ebc2d194
VertexC <bob2420083992@gmail.com>
2021-02-04 15:17:39 +0800

    update onnx models download script

--- onnx_models/download.sh
+++ onnx_models/download.sh
@@ -1,1 +1,16 @@
-wget https://github.com/onnx/models/raw/master/vision/classification/resnet/model/resnet50-caffe2-v1-9.onnx
+#!/bin/bash
+SCRIPT=$(readlink -f "$0")
+DIR=$(dirname "$SCRIPT")
+echo $DIR
+if [ ! -f ${DIR}/resnet50.onnx ]; then
+    wget -O ${DIR}/resnet50.onnx https://github.com/onnx/models/raw/master/vision/classification/resnet/model/resnet50-caffe2-v1-9.onnx
+fi
+if [ ! -f ${DIR}/mobilenet.onnx ]; then
+    wget -O ${DIR}/mobilenet.onnx https://github.com/onnx/models/blob/master/vision/classification/mobilenet/model/mobilenetv2-7.onnx
+fi
+if [ ! -f ${DIR}/vgg16.onnx ]; then
+    wget -O ${DIR}/vgg16.onnx https://github.com/onnx/models/blob/master/vision/classification/vgg/model/vgg16-7.onnx
+fi
+if [ ! -f ${DIR}/inception.onnx ]; then
+    wget -O ${DIR}/inception.onnx https://github.com/onnx/models/blob/master/vision/classification/inception_and_googlenet/inception_v2/model/inception-v2-9.onnx
+fi
