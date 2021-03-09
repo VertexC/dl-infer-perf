cd8bde9cf278d615f49eee850e17b216d992b129
VertexC <bob2420083992@gmail.com>
2021-03-09 17:02:16 +0800

    make tf->trt always regenerate model file

--- infer_perf/tf2trt.py
+++ infer_perf/tf2trt.py
@@ -17,18 +17,19 @@ def tf2trt_runner(model_name, batch_size=1):
         model.save(model_path)
 
     trt_path = model_path + '_TFTRT_FP32'
-    if not os.path.isdir(trt_path):
-        print('Converting to TF-TRT FP32...')
-        conversion_params = trt.DEFAULT_TRT_CONVERSION_PARAMS._replace(
-            precision_mode=trt.TrtPrecisionMode.FP32,
-            max_workspace_size_bytes=8000000000)
 
-        converter = trt.TrtGraphConverterV2(
-            input_saved_model_dir=model_path,
-            conversion_params=conversion_params)
-        converter.convert()
-        converter.save(output_saved_model_dir=trt_path)
-        print('Done Converting to TF-TRT FP32')
+    # if not os.path.isdir(trt_path):
+    # always regenerate model to avoid incompatibility between different onnx/trt version
+    print('Converting to TF-TRT FP32...')
+    conversion_params = trt.DEFAULT_TRT_CONVERSION_PARAMS._replace(
+        precision_mode=trt.TrtPrecisionMode.FP32,
+        max_workspace_size_bytes=8000000000)
+
+    converter = trt.TrtGraphConverterV2(input_saved_model_dir=model_path,
+                                        conversion_params=conversion_params)
+    converter.convert()
+    converter.save(output_saved_model_dir=trt_path)
+    print('Done Converting to TF-TRT FP32')
 
     saved_model_loaded = tf.saved_model.load(trt_path,
                                              tags=[tag_constants.SERVING])
@@ -51,10 +52,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="benchmark of tf/TensorRT")
     parser.add_argument("model", help="tf model name")
     parser.add_argument("--batch", type=int, default=1, help='batch size')
-    parser.add_argument("--size", type=int, default=1, help='test data size')
+    parser.add_argument("--size", type=int, default=256, help='test data size')
     args = parser.parse_args()
 
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
     runner = tf2trt_runner(args.model, batch_size=args.batch)
-    duration = util.simple_bench(runner, args.size)
-    print(duration)
+    throughput = util.simple_bench(runner,
+                                   data_size=args.size,
+                                   warmup=1,
+                                   rounds=5,
+                                   verbose=True)
+    print(throughput)
