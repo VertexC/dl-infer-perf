cd832d9a3ac3cd0b6714ae271e1f865e4b497cdf
VertexC <bob2420083992@gmail.com>
2021-03-29 17:44:04 +0800

    init bm_manager

---
+++ bm_manager/README.md
@@ -0,0 +1,2 @@
+## Fields
+optimizer,fe,model,batch_size,device,metric,group,timestamp,
---
+++ bm_manager/proto/proto.sh
@@ -0,0 +1,1 @@
+python -m grpc_tools.protoc -I ./proto --python_out=pkg --grpc_python_out=pkg ./proto/benchmark/benchmark.proto
