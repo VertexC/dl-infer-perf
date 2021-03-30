3527cbbb85e058251dbacd6542e53002ca025fd0
VertexC <bob2420083992@gmail.com>
2021-03-30 14:20:30 +0800

    make bm_manage detect duplicated metric

--- bm_manager/client.py
+++ bm_manager/client.py
@@ -5,24 +5,29 @@ import grpc
 import proto.pkg.benchmark.benchmark_pb2 as bmpb
 import proto.pkg.benchmark.benchmark_pb2_grpc as bmrpc
 
-def lunch_client(file, server, group):
-    print('Try to update benchmark {} to server {}'.format(file, server))
-    df = pd.read_csv(file)
-    print(df)
+def update_request(df, server, group):
+    print('Uploading df as group <{}>\n : {}'.format(group, df))
+    df['group'] = pd.Series([group]*len(df), index=df.index)
     df_bytes = pickle.dumps(df)
-
     with grpc.insecure_channel(server) as channel:
         stub = bmrpc.UpdateBenchmarkServiceStub(channel)
         response = stub.UpdateBenchmark(bmpb.BenchmarkByteUpdateRequest(data=df_bytes, group=group))
     print("Client received: {}".format(response.message))
 
+
+def update_file(file, server, group):
+    print('Try to update benchmark {} to server {}'.format(file, server))
+    df = pd.read_csv(file)
+    update_request(df, server, group)
+
+
 if __name__ == '__main__':
     import argparse
-    parser = argparse.ArgumentParser(description="benchmark client")
+    parser = argparse.ArgumentParser(description='update file to server')
     parser.add_argument("file", type=str, help="json file of tasks")
     parser.add_argument('server', type=str, default='localhost:50051', help='server url')
     parser.add_argument('group', type=str, default='test', help='grou name of benchmark')
 
     args = parser.parse_args()
 
-    lunch_client(args.file, args.server, args.group)
+    update_file(args.file, args.server, args.group)
--- bm_manager/server.py
+++ bm_manager/server.py
@@ -1,5 +1,7 @@
 import yaml
 import os
+import pickle
+import threading
 
 from concurrent import futures
 import grpc
@@ -19,55 +21,90 @@ def mkdir_p(d):
             pass
         else: raise
 
+'''
+df: pd.dataframe
+fields: {column_name: value}
+
+return: the index of df satisfy match fields, -1 if not found
+'''
+def query(df, fields):
+    if len(fields) == 0:
+        return df.index
+    else:
+        k,v = fields[0]
+        df = df[df[k] == v]
+        if len(df) == 0:
+            return None
+        else:
+            return query(df, fields[1:])
+
+
 class UpdateBenchmarkService(bmrpc.UpdateBenchmarkService):
-    
     def __init__(self, db):
         super(UpdateBenchmarkService, self).__init__()
         self.db = db
         
     def UpdateBenchmark(self, request, context):
-        msg = db.update(request.data)
-        return bmpb.BenchmarkByteUpdateReply(message='{}' % msg)
+        print('Receive UpdateBenchmark request')
+        df = pickle.loads(request.data)
+        print('Server get df\n:{}'.format(df))
+        msg = self.db.update(df)
+        if msg:
+            message = 'Failed to udpate: {}'.format(msg)
+        else:
+            message = 'Update Success'
+        return bmpb.BenchmarkByteUpdateReply(message=message)
 
 class BenchmarkDB:
-    def __init__(self, url, fields):
-        self.url = ''
-        self.fields = ''
-        print(url, fields)
-        self.df = None
-        self._set_up(url, fields)
+    def __init__(self, url, key_f, val_f):
+        self.url = url
+        self.key_f = key_f
+        self.val_f = val_f
+        self._df = None
+        self._set_up(url, key_f | val_f)
+        self._lock = threading.Lock()
 
     def _set_up(self, url, fields):
         print("Set up metric_db from {}".format(url))
-        self.url = url
         if os.path.isfile(url):
             df = pd.read_csv(url)
         else:
-            data = {k:[v] for k,v in fields.items()}
+            data = {k:None for k in fields}
             df = pd.DataFrame.from_dict(data)
             mkdir_p(os.path.dirname(url))
             df.to_csv(url, index=False)
-        self.df = df
+        self._df = df
     
-    def update(self, data):
-        return 'test'
-
+    def update(self, df):
+        print('Checking format...')
+        import pdb; pdb.set_trace()
+        headers = df.head()
+        if set(headers) != set(self.key_f | self.val_f):
+            return 'headers not match, expect {}, got {}'.format(self.key_f | self.val_f, headers)
+        self._lock.acquire()
+        to_drop = []
+        for _, r in df.iterrows():
+            row = [(k, r[k]) for k in self.key_f]
+            index = query(self._df, row)
+            if index is not None:
+                to_drop += index.tolist()
+        self._df = self._df.drop(to_drop)
+        self._df = pd.concat([self._df, df], ignore_index=True)
+        self._lock.release()
+        print('Data updated:\n {}'.format(self._df))
+        return None
 
 def lunch_server(config_file):
     with open(config_file) as f:
         content = yaml.load(f, Loader=yaml.FullLoader)
-    assert 'db_url' in content and 'fields' in content
     url = content['db_url']
-    fields_list = content['fields']
-    fields_dict = {}
-    for fields in fields_list:
-        key = fields['key']
-        val = fields['default']
-        fields_dict[key] = val
+    key_list = content['key_fields']
+    val_list = content['val_fields']
+    key_f = set(key_list.split(','))
+    val_f = set(val_list.split(','))
+    bm_db = BenchmarkDB(url, key_f, val_f)
 
-    bm_db = BenchmarkDB(url, fields_dict)
-
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     bmrpc.add_UpdateBenchmarkServiceServicer_to_server(UpdateBenchmarkService(bm_db), server)
     server.add_insecure_port('[::]:50051')
     server.start()
